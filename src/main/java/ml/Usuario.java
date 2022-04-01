3322e4f920c693b29503fbf323eaac074bdd63f7
TrillesP <trillessky@gmail.com>
2022-04-01 16:44:38 -0300

    selenium
    chrome driver
    taking screenshot, saving file
    testing search button
    wait
    testing if search was done correctly
    testing to see if finds specific smartphone in page by partialLinkText and .contains

--- src/main/java/ml/Usuario.java
+++ src/main/java/ml/Usuario.java
@@ -2,12 +2,20 @@ package ml;
 
 
 import java.io.File;
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
 import org.apache.commons.io.FileUtils;
+import org.hamcrest.CoreMatchers;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.*;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.junit.*;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static java.time.temporal.ChronoUnit.SECONDS;
 
 
 public class Usuario {
@@ -40,8 +48,23 @@ public class Usuario {
             driver.findElement(By.name("as_word")).clear();
             driver.findElement(By.name("as_word")).sendKeys("smartphone");
             driver.findElement(By.cssSelector("button.nav-search-btn")).click();
+            WebDriverWait wait = new WebDriverWait(driver, Duration.of(3,SECONDS));
+            try {
+                wait.until(ExpectedConditions.titleContains("Smartphone"));
+            } catch(TimeoutException ex) {
+                Assert.fail("Demorou mais de 3 segundos pra carregar a página.");
+            }
             print();
             Assert.assertEquals("Smartphone",driver.findElement(By.cssSelector("h1.ui-search-breadcrumb__title")).getText());
+            String frase = "128 GB";
+            if (driver.findElement(By.partialLinkText("Samsung Galaxy A52")).getText().contains(frase))
+            {
+                System.out.println("Achou.");
+            }
+            else{
+                System.out.println("Erro.");
+            }
         }
+
 
 }
