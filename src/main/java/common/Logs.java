aee00b6c73ad3bad5145fb9d3e5f33629b07f1f4
TrillesP <trillessky@gmail.com>
2022-04-04 13:49:04 -0300

    selenium
    chrome driver
    taking screenshot, saving file
    testing search button
    wait
    testing if search was done correctly
    testing to see if finds specific smartphone in page by partialLinkText and .contains
    
    creating another class for common usage of printscreens, formatting folders with date and time and different print names.
    
    creating log file with date/time. append file, formatting time with milliseconds.

---
+++ src/main/java/common/Logs.java
@@ -0,0 +1,44 @@
+package common;
+
+import com.opencsv.CSVWriter;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+public class Logs {
+    String dataLog;
+
+    public void registrarCabecalho(String dataLog) throws IOException{
+        String[] cabecalho = {"data e hora", "caso de teste", "mensagem"};
+        this.dataLog = dataLog;
+        Writer escritor = Files.newBufferedWriter(Paths.get("target/logs/log - "+dataLog+".csv"));
+        CSVWriter escritorCSV = new CSVWriter(escritor);
+
+        escritorCSV.writeNext(cabecalho);
+
+
+        escritorCSV.flush();
+        escritor.close();
+    }
+    public void registrarCSV(String casoDeTeste, String mensagem) throws IOException{
+        String dataHoraLog = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss:SSS").format(Calendar.getInstance().getTime());
+        String[] linha = new String[]{dataHoraLog, casoDeTeste, mensagem};
+
+        FileWriter escritor = new FileWriter("target/logs/log - "+dataLog+".csv", true);
+
+        CSVWriter escritorCSV = new CSVWriter(escritor);
+
+        escritorCSV.writeNext(linha);
+
+        escritorCSV.flush();
+        escritor.close();
+
+    }
+}
--- src/main/java/ml/Usuario.java
+++ src/main/java/ml/Usuario.java
@@ -1,12 +1,12 @@
 package ml;
 
 
-import java.io.File;
 import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.util.Calendar;
 
 import common.Evidencias;
+import common.Logs;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.*;
 import org.junit.*;
@@ -20,6 +20,7 @@ public class Usuario {
         public String url;
         public WebDriver driver;
 
+        Logs logs;
         Evidencias evidencias;
         static String dataHora = new SimpleDateFormat("yyyy-MM-dd HH-mm").format(Calendar.getInstance().getTime());
 
@@ -29,6 +30,7 @@ public class Usuario {
             System.setProperty("webdriver.chrome.driver", "drivers/chrome/99/chromedriver.exe");
             driver = new ChromeDriver();
             evidencias = new Evidencias();
+            logs = new Logs();
         }
 
         @After
@@ -39,11 +41,16 @@ public class Usuario {
         @Test
         public void buscar() throws Exception {
             String casoDeTeste = "Pesquisa Mercado Livre";
+            String dataLog = new SimpleDateFormat("yyyy-MM-dd HH-mm").format(Calendar.getInstance().getTime());
+            logs.registrarCabecalho(dataLog);
             driver.navigate().to(url);
+            logs.registrarCSV(casoDeTeste, "Abriu navegador.");
             driver.findElement(By.name("as_word")).clear();
             driver.findElement(By.name("as_word")).sendKeys("smartphone");
+            logs.registrarCSV(casoDeTeste, "Digitou pesquisa.");
             evidencias.print(driver,dataHora,casoDeTeste, "Digitou pesquisa");
             driver.findElement(By.cssSelector("button.nav-search-btn")).click();
+            logs.registrarCSV(casoDeTeste, "Clicou na lupa.");
             WebDriverWait wait = new WebDriverWait(driver, Duration.of(3,SECONDS));
             try {
                 wait.until(ExpectedConditions.titleContains("Smartphone"));
@@ -51,14 +58,18 @@ public class Usuario {
                 Assert.fail("Demorou mais de 3 segundos pra carregar a página.");
             }
             evidencias.print(driver,dataHora,casoDeTeste, "Pesquisa realizada");
+            logs.registrarCSV(casoDeTeste, "Carregou a página.");
             Assert.assertEquals("Smartphone",driver.findElement(By.cssSelector("h1.ui-search-breadcrumb__title")).getText());
+            logs.registrarCSV(casoDeTeste, "Confirmou pesquisa de Smartphones.");
             String frase = "128 GB";
             if (driver.findElement(By.partialLinkText("Samsung Galaxy A52")).getText().contains(frase))
             {
                 System.out.println("Achou.");
+                logs.registrarCSV(casoDeTeste, "Achou Samsung Galaxy A52 de 128GB");
             }
             else{
                 System.out.println("Erro.");
+                logs.registrarCSV(casoDeTeste, "Não achou o celular.");
             }
         }
 
