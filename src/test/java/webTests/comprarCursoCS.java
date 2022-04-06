21094b7556c8c40b92c402c184cd7a17051f8d6a
TrillesP <trillessky@gmail.com>
2022-04-05 21:42:48 -0300

    selenium
    chrome driver
    taking screenshot, saving file
    testing search button
    wait
    testing if search was done correctly
    testing to see if finds specific smartphone in page by partialLinkText and .contains
    
    creating another class for common usage of printscreens, formatting folders with date and time and different print names.
    
    creating log file with date/time. append file, formatting time with milliseconds.
    
    tested BDD with cucumber+selenium to open website, search and check prices for classes.
    
    optimized code, testing Scenario Outline with examples, mostly stopped using Thread.sleep

--- src/test/java/webTests/comprarCursoCS.java
+++ src/test/java/webTests/comprarCursoCS.java
@@ -5,7 +5,6 @@ import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import org.junit.Assert;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -13,10 +12,13 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 public class comprarCursoCS {
 
     WebDriver driver;
-
+    WebDriverWait wait;
 
 
     @Before
@@ -24,7 +26,7 @@ public class comprarCursoCS {
         System.setProperty("webdriver.chrome.driver", "drivers/chrome/99/chromedriver.exe");
         driver = new ChromeDriver();
         driver.manage().window().maximize();
-
+        wait = new WebDriverWait(driver,Duration.ofSeconds(5),Duration.ofMillis(1));
     }
 
 
@@ -36,12 +38,7 @@ public class comprarCursoCS {
 
     @When("^I search for \"([^\"]*)\" and click on search button$")
     public void i_search_for_and_click_on_search_button(String curso) throws InterruptedException {
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(3));
-        try {
-            wait.until(ExpectedConditions.titleContains("Cursos"));
-        } catch(TimeoutException ex) {
-            Assert.fail("Demorou mais de 3 segundos pra carregar a página.");
-        }
+        wait.until(ExpectedConditions.titleContains("Cursos"));
         driver.findElement(By.id("auto-2")).click();
         driver.findElement(By.id("auto-2")).clear();
         Thread.sleep(3000);
@@ -50,10 +47,10 @@ public class comprarCursoCS {
     }
 
     @Then("^I see the search results for \"([^\"]*)\"$")
-    public void i_see_the_search_results_for(String curso) throws InterruptedException {
-        Thread.sleep(3000);
+    public void i_see_the_search_results_for(String curso) {
         WebElement teste = new WebDriverWait(driver, Duration.ofSeconds(3)).until(driver -> driver.findElement(By.cssSelector("h3.content-card-name")));
-        Assert.assertEquals(teste.getText(),"Exame da "+curso+"-AT - ISTQB");
+        wait.until(ExpectedConditions.textToBePresentInElement(teste,curso));
+        assertThat(teste.getText(), containsString(curso));
         System.out.println("3 - Mostrou resultados da pesquisa.");
     }
 
@@ -64,15 +61,10 @@ public class comprarCursoCS {
     }
 
     @Then("^I confirm the Class' name as \"([^\"]*)\" and it's price as \"([^\"]*)\"$")
-    public void i_confirm_the_Class_name_as_and_it_s_price_as(String nome, String preco) throws InterruptedException {
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(3));
-        try {
-            wait.until(ExpectedConditions.titleContains("Exame da CTFL-AT - ISTQB"));
-        } catch(TimeoutException ex) {
-            Assert.fail("Demorou mais de 3 segundos pra carregar a página.");
-        }
-        Thread.sleep(3000);
-        Assert.assertEquals(preco,driver.findElement(By.cssSelector("p.content-price-installments-amount")).getText());
+    public void i_confirm_the_Class_name_as_and_it_s_price_as(String nome, String preco) {
+        WebElement teste = new WebDriverWait(driver, Duration.ofSeconds(3)).until(driver -> driver.findElement(By.cssSelector("p.content-price-installments-amount")));
+        wait.until(ExpectedConditions.titleContains(nome));
+        wait.until(ExpectedConditions.textToBePresentInElement(teste,preco));
         System.out.println("5 - Confirmou que o nome da classe é "+nome+" e o preço é "+preco+".");
     }
 
--- src/test/resources/features/comprarCursoCS.feature
+++ src/test/resources/features/comprarCursoCS.feature
@@ -5,3 +5,14 @@ Feature: Purchase Class CS
     Then I see the search results for "CTFL"
     When I click on class name
     Then I confirm the Class' name as "Exame da CTFL-AT - ISTQB" and it's price as "R$ 104,17"
+
+  Scenario Outline: Search for Classes and Add to Cart
+    Given I access Iterasys website
+    When I search for <curso> and click on search button
+    Then I see the search results for <curso>
+    When I click on class name
+    Then I confirm the Class' name as <curso> and it's price as <preco>
+    Examples:
+      | curso   | preco       |
+      | "CTFL"  | "R$ 104,17" |
+      | "Java"  | "R$ 24,83"  |
